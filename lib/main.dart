a3962ddcb42906c2e722ff197e19c879fcd8dfc6
matthew <minwoo6263@gmail.com>
2024-12-12 12:47:45 +0900

    파일 구조 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,90 @@
-import 'package:flutter/material.dart';
-
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sea U',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        highlightColor: Colors.orange,
+        appBarTheme: AppBarTheme(
+          centerTitle: false,
+          titleTextStyle: TextStyle(
+            fontSize: 18,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            foregroundColor: WidgetStatePropertyAll(Colors.white),
+            backgroundColor: WidgetStatePropertyAll(Colors.orange),
+            minimumSize: WidgetStatePropertyAll(Size.fromHeight(50)),
+            textStyle: WidgetStatePropertyAll(
+              TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            shape: WidgetStatePropertyAll(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey,
+          ),
+          contentPadding: EdgeInsets.symmetric(
+            vertical: 16,
+            horizontal: 20,
+          ),
+          border: MaterialStateOutlineInputBorder.resolveWith((states) {
+            // print(states);
+            // 1. states Set 안에 WidgetState.focused 가 포함이 되어있을때
+            if (states.contains(WidgetState.focused)) {
+              return OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(
+                  color: Colors.grey,
+                  width: 2,
+                ),
+              );
+            }
+            WidgetState;
+            // 2. states Set 안에 WidgetState.error 가 포함이 되어있을때
+            if (states.contains(WidgetState.error)) {
+              return OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(
+                  color: Colors.red[100]!,
+                  width: 2,
+                ),
+              );
+            }
+
+            // 3. 디폴트값
+            return OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: BorderSide(
+                color: Colors.grey[300]!,
+                width: 1,
+              ),
+            );
+          }),
+        ),
+      ),
+      home: WelcomePage(),
+      // home: HomePage(),
+      // home: ChatDetailPage(),
     );
   }
 }
