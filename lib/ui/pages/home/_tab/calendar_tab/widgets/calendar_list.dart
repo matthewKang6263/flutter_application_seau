47b85e869a2f04df31b53942521eb3300650a10e
duddms72003 <duddms72003@naver.com>
2024-12-16 20:38:01 +0900

    feat<calendar>: 일정 삭제 기능 추가

--- lib/ui/pages/home/_tab/calendar_tab/widgets/calendar_list.dart
+++ lib/ui/pages/home/_tab/calendar_tab/widgets/calendar_list.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 class CalendarList extends StatelessWidget {
   final List<Map<String, String>> savedItems;
+  final Function(String) onDelete;
 
   const CalendarList({
     Key? key,
     required this.savedItems,
+    required this.onDelete,
   }) : super(key: key);
 
   @override
@@ -16,105 +18,126 @@ class CalendarList extends StatelessWidget {
         itemCount: savedItems.length,
         itemBuilder: (context, index) {
           final item = savedItems[index];
-          return Card(
-            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 6),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-              side: BorderSide(
-                color: Color.fromRGBO(217, 217, 217, 1),
-                width: 1,
+          return Dismissible(
+            key: UniqueKey(),
+            direction: DismissDirection.endToStart,
+            onDismissed: (direction) {
+              // 여기에 삭제 로직을 추가하세요
+              onDelete(item['id'] ?? '');
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('일정이 삭제되었습니다')),
+              );
+            },
+            background: Container(
+              color: Colors.red,
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Icon(Icons.delete, color: Colors.white),
+                ),
               ),
             ),
-            elevation: 0,
-            color: Colors.white,
-            child: Padding(
-              padding: EdgeInsets.all(16),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Column(
-                    children: [
-                      Text(
-                        item['weekday'] ?? '',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        item['day'] ?? '',
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(width: 20),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+            child: Card(
+              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+                side: BorderSide(
+                  color: Color.fromRGBO(217, 217, 217, 1),
+                  width: 1,
+                ),
+              ),
+              elevation: 0,
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
                       children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              '나의 버디: ${item['name'] ?? ''}',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              '확정됨',
-                              style: TextStyle(
-                                color: Colors.blue,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ],
+                        Text(
+                          item['weekday'] ?? '',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                        SizedBox(height: 7),
-                        Row(
-                          children: [
-                            Icon(Icons.calendar_today,
-                                size: 16, color: Colors.grey),
-                            SizedBox(width: 8),
-                            Text(
-                              item['date'] ?? '',
-                              style: TextStyle(fontSize: 14),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 4),
-                        Row(
-                          children: [
-                            Icon(Icons.access_time,
-                                size: 16, color: Colors.grey),
-                            SizedBox(width: 8),
-                            Text(
-                              item['time'] ?? '',
-                              style: TextStyle(fontSize: 14),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 4),
-                        Row(
-                          children: [
-                            Icon(Icons.location_on,
-                                size: 16, color: Colors.grey),
-                            SizedBox(width: 8),
-                            Text(
-                              item['location'] ?? '',
-                              style: TextStyle(fontSize: 14),
-                            ),
-                          ],
+                        Text(
+                          item['day'] ?? '',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                ],
+                    SizedBox(width: 20),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                '나의 버디: ${item['name'] ?? ''}',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                '확정됨',
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 7),
+                          Row(
+                            children: [
+                              Icon(Icons.calendar_today,
+                                  size: 16, color: Colors.grey),
+                              SizedBox(width: 8),
+                              Text(
+                                item['date'] ?? '',
+                                style: TextStyle(fontSize: 14),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 4),
+                          Row(
+                            children: [
+                              Icon(Icons.access_time,
+                                  size: 16, color: Colors.grey),
+                              SizedBox(width: 8),
+                              Text(
+                                item['time'] ?? '',
+                                style: TextStyle(fontSize: 14),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 4),
+                          Row(
+                            children: [
+                              Icon(Icons.location_on,
+                                  size: 16, color: Colors.grey),
+                              SizedBox(width: 8),
+                              Text(
+                                item['location'] ?? '',
+                                style: TextStyle(fontSize: 14),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           );
