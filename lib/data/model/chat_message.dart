4f8b84fcffef74b249c5928eb51b11ecb5f88c50
GitHub eso21309 <GitHub eso21309@gmail.com>
2024-12-16 10:26:48 +0900

    <refactor>(model)chat_message 추가

--- lib/data/model/chat_message.dart
+++ lib/data/model/chat_message.dart
@@ -1,1 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class ChatMessage {
+  final String content;
+  final String senderId;
+  final DateTime timestamp;
+  final bool isRead;
+
+  ChatMessage({
+    required this.content,
+    required this.senderId,
+    required this.timestamp,
+    required this.isRead,
+  });
+
+  factory ChatMessage.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return ChatMessage(
+      content: data["content"] ?? "",
+      senderId: data['senderId'] ?? '',
+      timestamp: (data['timestamp'] as Timestamp).toDate(),
+      isRead: data['isRead'] ?? false,
+    );
+  }
+}
