4e5a88d3f7a003f6c6a2109484a9ba42da3f289e
GitHub eso21309 <GitHub eso21309@gmail.com>
2024-12-16 10:24:46 +0900

    <refactor>(model/user) id 데이터 추가

--- lib/data/model/user.dart
+++ lib/data/model/user.dart
@@ -3,12 +3,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class User {
   // 기본 유저 정보
   final String uid; // Firebase 문서 ID
-
+  final String id; //사용자 아이디
   final String nickname; // 닉네임(화면에 표시될 이름)
 
   // 생성자
   User({
     required this.uid,
+    required this.id,
     required this.nickname,
   });
 
@@ -19,7 +20,7 @@ class User {
 
     return User(
       uid: doc.id, // 문서 ID를 uid로 사용
-
+      id: data['id'],
       nickname: data['nickname'], // 닉네임 필드
     );
   }
@@ -27,6 +28,7 @@ class User {
   // User 객체를 Firestore에 저장할 수 있는 Map으로 변환
   Map<String, dynamic> toFirestore() {
     return {
+      "id": id,
       'nickname': nickname,
     };
   }
