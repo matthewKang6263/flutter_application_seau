7e6dd5f80063ebeb26a7370732287d857e12ffa4
akeetuitui <skj6164@naver.com>
2024-12-16 15:53:55 +0900

    <refactor>(home_indxed): mypage연결

--- lib/ui/pages/home/widgets/home_indexed_stack.dart
+++ lib/ui/pages/home/widgets/home_indexed_stack.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_seau/ui/pages/calendar/calendar_page.dart';
 import 'package:flutter_application_seau/ui/pages/chat/chat_list_page.dart';
 import 'package:flutter_application_seau/ui/pages/home/_tab/calendar_tab/calendar_tab.dart';
 
 import 'package:flutter_application_seau/ui/pages/home/_tab/home_tab/home_tab.dart';
 import 'package:flutter_application_seau/ui/pages/home/_tab/my_tab/my_tab.dart';
 import 'package:flutter_application_seau/ui/pages/home/home_view_model.dart';
+import 'package:flutter_application_seau/ui/pages/mypage/my_page.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -18,9 +20,9 @@ class HomeIndexedStack extends StatelessWidget {
           index: currentIndex,
           children: [
             HomeTab(),
-            CalendarTab(),
+            CalendarPage(),
             ChatListPage(), //chatTap 페이지 삭제하고 여기서 바로 chatListPage로 연결되도록 수정함
-            MyTab(),
+            MyPage(),
           ],
         );
       },
